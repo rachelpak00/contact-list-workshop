a48191526489350995e6a6678de0cb72eafbdfe9
Rachel Pak <rachelpak00@gmail.com>
2023-06-06 23:45:25 -0500

    last commit

--- contactList/src/components/SelectedContact.jsx
+++ contactList/src/components/SelectedContact.jsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-import React from "react";
->>>>>>> origin/main
 import { useState, useEffect } from "react";
 
 const SelectedContact = ({selectedContactId, setSelectedContactId}) => {
